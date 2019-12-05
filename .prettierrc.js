d16db432ebf2a83ba989cc189c054fd69a34ad14
woaizimmy <337442628@qq.com>
2019-12-05 17:12:04 +0800

    尝试使用@umijs/fabric

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,18 @@
+const fabric = require('@umijs/fabric');
+
 module.exports = {
-  printWidth: 80, // 换行字符串阈值
-  semi: true, // 句末加分号
-  singleQuote: true, // 用单引号
-  trailingComma: 'none', // 最后一个对象元素加逗号
-  bracketSpacing: true, // 对象，数组加空格
-  jsxBracketSameLine: false, // jsx > 是否另起一行
-  arrowParens: 'always', // (x) => {} 是否要有小括号
-  requirePragma: false, // 是否要注释来决定是否格式化代码
-  proseWrap: 'preserve', // 是否要换行
-  tabWidth: 2 //缩进
+  ...fabric.prettier,
 };
+
+// module.exports = {
+//   printWidth: 80, // 换行字符串阈值
+//   semi: true, // 句末加分号
+//   singleQuote: true, // 用单引号
+//   trailingComma: 'none', // 最后一个对象元素加逗号
+//   bracketSpacing: true, // 对象，数组加空格
+//   jsxBracketSameLine: false, // jsx > 是否另起一行
+//   arrowParens: 'always', // (x) => {} 是否要有小括号
+//   requirePragma: false, // 是否要注释来决定是否格式化代码
+//   proseWrap: 'preserve', // 是否要换行
+//   tabWidth: 2 //缩进
+// };
